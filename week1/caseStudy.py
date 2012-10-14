7d2342b37fe1cc6c877126c47d5318e0d24a5500
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2012-10-13 23:23:25 -0300

    created homework dir

---
+++ week1/caseStudy.py
@@ -0,0 +1,10 @@
+import ipdb
+
+import math
+
+def rec_russian(a, b):
+    if a == 0: return 0
+    if a % 2 == 0: return 2*rec_russian(a/2, b)
+    return b + 2*rec_russian((a-1)/2, b)
+
+print rec_russian(4,5)
